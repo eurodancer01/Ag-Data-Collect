8c682eaabe4ff4d35bcd9d69919fe3094658fd9f
eurodancer01 <92173505+eurodancer01@users.noreply.github.com>
2024-06-30 02:06:49 -0700

    Update data_storage.py

--- Ag-Data-Collect/src/data_storage.py
+++ Ag-Data-Collect/src/data_storage.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
-def save_to_csv(dataframe, filepath):
+def save_to_csv(dataframe, filepath): #Convert the data extracted to a .csv file
     dataframe.to_csv(filepath, index=False)
 
